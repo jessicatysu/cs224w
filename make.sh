6dcee199787a77b86d0b4fe249e5931682070ef0
Jessica Su <jessica@dn0a2313db.sunet>
2013-11-11 14:36:04 -0800

    first commit

---
+++ make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+latex -src -interaction=nonstopmode main.tex
+bibtex main.aux
+latex -src -interaction=nonstopmode main.tex
+dvips -o main.ps main.dvi
+ps2pdf main.ps
+rm -r *.log
+rm -r *.aux
+rm -r *.blg
